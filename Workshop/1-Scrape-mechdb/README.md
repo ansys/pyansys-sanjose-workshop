2a49fe3c2fbff0a5ce9fce07eaf2547cd8f1b89e
Mohamed Koubaa <koubaa@github.com>
2024-06-04 13:44:59 -0500

    workshop 1

---
+++ Workshop/1-Scrape-mechdb/README.md
@@ -0,0 +1,15 @@
+# Workshop 1: Scrap mechdb files
+
+# Problem statement
+There is a problem at your organization, an important simulation model does not match experiments. After three weeks of intense investigation, a senior analyst has discovered the cause, and now make the following recommendations:
+
+- The `Weak Springs` option  under `Analysis Settings` has to be set to [No], for applicable analyses
+- Both `Beam Connections` and `Bearings` can not use a [Rigid] scoping, whether it is applied directly via Geometry Selection or indirectly using a remote point
+
+The analyst has identified a set of projects that may be affected. There are thousands of them in the PLM system. They would like for you to check for these problems in all of these projects, and report back which of them have which problem.
+
+# Workspace
+See `sample.py` for an outline of a python program. In the `Files` folder, there are some sample projects that may contain the above problems. Modify `sample.py` so that it prints out which projects are affected by which problems. Once you have done this, you can use it on the thousands of projects in the PLM system.
+
+# Expected result
+TODO
---
+++ Workshop/1-Scrape-mechdb/sample.py
@@ -0,0 +1,61 @@
+import os
+import typing
+
+import ansys.mechanical.core as mech
+app=mech.App(version=241)
+globals().update(mech.global_variables(app, True))
+FILES_PATH = os.path.join(__file__, os.pardir, "Files")
+
+class Problems:
+    RIGID_REMOTE_POINT_ON_BEAM_CONNECTION = """
+A beam connection uses a rigid connection!
+"""
+
+    RIGID_REMOTE_POINT_ON_BEARING = """
+A bearing uses a rigid connection!
+"""
+
+    WEAK_SPRINGS_ENABLED = """
+An analysis has weak springs enabled!
+"""
+
+def has_weak_springs():
+    return False
+
+def has_rigid_beam():
+    return False
+
+def has_rigid_bearing():
+    return False
+
+def get_problems(mechdb: str) -> typing.List[str]:
+    app.open(os.path.join(FILES_PATH, mechdb))
+    problems = []
+    if (has_weak_springs()):
+        problems.append(Problems.WEAK_SPRINGS_ENABLED)
+    if (has_rigid_beam()):
+        problems.append(Problems.RIGID_REMOTE_POINT_ON_BEAM_CONNECTION)
+    if (has_rigid_bearing()):
+        problems.append(Problems.RIGID_REMOTE_POINT_ON_BEARING)
+    return problems
+
+def report_problem(mechdb: str, problem: str):
+    print(f"{mechdb} has a problem: {problem}")
+
+def report_no_problem(mechdb: str):
+    print(f"{mechdb} has no problems")
+
+def test_mechdbs():
+    mechdbs = [file for file in os.listdir(FILES_PATH) if file.endswith(".mechdb")]
+    for mechdb in mechdbs:
+        problems = get_problems(os.path.join(mechdb))
+        if len(problems) == 0:
+            report_no_problem(mechdb)
+        for problem in problems:
+            report_problem(mechdb, problem)
+
+if __name__ == "__main__":
+    try:
+        test_mechdbs()
+    finally:
+        app.new()  # This ensures that there are no leftover lock files
