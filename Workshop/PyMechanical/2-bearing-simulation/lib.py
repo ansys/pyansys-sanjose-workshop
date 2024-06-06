5969b8a8cdd57a25c713567e0f52ffbe40d9ecb3
Mohamed Koubaa <koubaa@github.com>
2024-06-06 07:58:02 -0500

    start

--- Workshop/PyMechanical/2-bearing-simulation/lib.py
+++ Workshop/PyMechanical/2-bearing-simulation/lib.py
@@ -4,7 +4,14 @@ import ansys.mechanical.core as mech
 
 def _export_image(app: mech.App, output_path: str) -> None:
     """Export the image the given output path."""
-    pass
+    image_settings = Ansys.Mechanical.Graphics.GraphicsImageExportSettings()
+    image_format = GraphicsImageExportFormat.PNG
+    image_settings.Resolution = GraphicsResolutionType.EnhancedResolution
+    image_settings.Background = GraphicsBackgroundType.White
+    image_settings.CurrentGraphicsDisplay=False
+    image_settings.Width = 720
+    image_settings.Height = 720
+    app.ExtAPI.Graphics.ExportImage(output_path, image_format, image_settings)
 
 
 def _print_output(app: mech.App, solution_time: float, deformation_max: float, deformation_min: float, deformation_stdev: float) -> None:
@@ -22,7 +29,19 @@ def _setup(app: mech.App, velocities: typing.List[float], input_dict: typing.Dic
     """
     # hint: the command to set a row of the rotational velocity Y component table looks like this:
     #           rotational_velocity.YComponent.Output.DiscreteValues[0]=Quantity(1, '[rad/s]')
-    pass
+    bearing = DataModel.GetObjectsByName("AUTO_BEARING")[0]
+    bearing.StiffnessK11.Output.SetDiscreteValue(0, Quantity(input_dict["k11"]))
+    bearing.StiffnessK22.Output.SetDiscreteValue(0, Quantity(input_dict["k22"]))
+    bearing.StiffnessK12.Output.SetDiscreteValue(0, Quantity(input_dict["k12"]))
+    bearing.StiffnessK21.Output.SetDiscreteValue(0, Quantity(input_dict["k21"]))
+    bearing.DampingC11.Output.SetDiscreteValue(0, Quantity(input_dict["c11"]))
+    bearing.DampingC22.Output.SetDiscreteValue(0, Quantity(input_dict["c22"]))
+    bearing.DampingC12.Output.SetDiscreteValue(0, Quantity(input_dict["c12"]))
+    bearing.DampingC21.Output.SetDiscreteValue(0, Quantity(input_dict["c21"]))
+
+    rotational_velocity = DataModel.GetObjectsByType(DataModelObjectCategory.RotationalVelocity)
+    for i in range(len(velocities)):
+        rotational_velocity.YComponent.Output.SetDiscreteValue(i, Quantity(velocities[i], "rad/s"))
 
 
 def _get_output(app) -> typing.Tuple[float, float, float, float]:
