63ca34a46082e5119f7c71aa874c75eae420b2a3
rs-bh-n <ramsobhan.kokkiligadda@ansys.com>
2024-06-06 19:28:14 +0530

    dpf solutions added

--- Workshop/PyDpf/2-dpf_challenge_thermal.py
+++ Workshop/PyDpf/2-dpf_challenge_thermal.py
@@ -84,36 +84,91 @@ from ansys.dpf.core.plotter import DpfPlotter
 import numpy as np
 
 
+
 # =========================================================================================
 # Load result file and create model database
 # =========================================================================================
 # Define Datasource using result file
 # Define Model using this datasource
-# your code
+
+path = r"inputs\transient_thermal.rth"
+
+ds = dpf.DataSources(path)
+my_model = dpf.Model(ds)
 
 
 # =========================================================================================
 ### 1. List Contents of Model and Plot Elemental Volume for Named Selection named "PCB"
 # =========================================================================================
-# your code
+# print(my_model)
+my_mesh = my_model.metadata.meshed_region
+get_NSs = my_model.metadata.available_named_selections
+my_mesh_scoping = my_model.metadata.named_selection("PCB")
+print(my_mesh_scoping)
+
+
+scoping_op = dpf.operators.mesh.from_scoping()
+scoping_op.inputs.scoping.connect(my_mesh_scoping)
+scoping_op.inputs.mesh.connect(my_mesh)
+my_mesh_ns = scoping_op.outputs.mesh()
+# print(my_mesh_ns)
+
+
+get_all_volumes = my_model.results.elemental_volume.on_all_time_freqs
+get_fieldContainers_volume = get_all_volumes(mesh_scoping=my_mesh_scoping).eval()
+get_field_volume = get_fieldContainers_volume[-1]
+# print(get_field_temperature)
+
+
+my_plot = DpfPlotter()
+my_plot.add_field(get_field_volume, my_mesh_ns)
+my_plot.show_figure(show_axes=True)
+print("Smallest Element's Volume: ", float(get_field_volume.data.min()), " m3")
+print("Largest Element's Volume: ", float(get_field_volume.data.max()), " m3")
+
+
 
 
 # =========================================================================================
 ### 2. Contact post processing  
 # =========================================================================================
-# your code
+
+my_mesh_scoping2 = my_model.metadata.named_selection("PCB_CONT_2")
+
+scoping_op2 = dpf.operators.mesh.from_scoping()
+scoping_op2.inputs.scoping.connect(my_mesh_scoping2)
+scoping_op2.inputs.mesh.connect(my_mesh)
+
+my_mesh_ns2 = scoping_op2.outputs.mesh()
+# print(my_mesh_ns)
+
+contact_heatFlow_op = dpf.operators.result.nmisc(
+    mesh_scoping=my_mesh_scoping2,
+    data_sources=ds,
+    item_index=98,
+)
+# print(contact_heatFlow_op)
+
+contact_heatFlow_res = contact_heatFlow_op.outputs.fields_container()
+# print(contact_heatFlow_res)
 
 
 # =========================================================================================
 ### 2B.  Plot HEAT FLOW Contour through the contact pair
 # =========================================================================================
-# your code
+
+sargs = dict(title="Contact Heat Flow [W]", title_font_size=30, label_font_size=20)
+my_plot = DpfPlotter()
+my_plot.add_field(contact_heatFlow_res[0], my_mesh_ns2, scalar_bar_args=sargs)
+my_plot.show_figure(show_axes=True)
 
 
 # =========================================================================================
 ### 2C. Sum up (using numpy) and Print TOTAL HEAT FLOW through a contact pair 
 # =========================================================================================
-# your code
+
+heat_flow = np.sum(np.array(contact_heatFlow_res[0].data))
+print("Total Heat Flow: ", round(heat_flow,2), " [W]")
 
 
 
